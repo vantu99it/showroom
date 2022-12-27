a6bac34f8135e5f404836bd95a1467f34fcbf2d6
nguyenbathanh97 <nguyenbathanh88888@gmail.com>
2022-12-27 22:12:29 +0700

    update

--- index.php
+++ index.php
@@ -1,48 +1,308 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Trang chủ</title>
-        <link rel="stylesheet" type="text/css"
-            href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-            crossorigin="anonymous"/>
-        <link rel="stylesheet" href="./lib/fontawesome/css/all.min.css">
-        <link rel="stylesheet" href="./css/style.css" />
-    </head>
 
-    <body>
-        <div class="header">
-            <div class="menu">
-                <div class="container">
-                    <ul>
-                        <li><a href="#">Trang chủ</a></li>
-                        <li><a href="#">Giới thiệu</a></li>
-                        <li><a href="#">Tin tức</a></li>
-                    </ul>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Trang chủ</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./lib/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+    <div class="header">
+        <div class="menu">
+            <div class="container">
+                <ul>
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">Tin tức</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="slider">
+            <div id="slider-in">
+                <img src="http://1.bp.blogspot.com/-6eD9x-eISTU/U9x_KcwZV1I/AAAAAAAAOnc/fllCSGEGb-g/s1600/Cover-facebook-mercedes-benz-6.jpg" alt="" />
+                <img src="https://lh4.googleusercontent.com/-AwuuI17Lkec/UUvNt-srWfI/AAAAAAAAGnM/x9ZDsWcBegY/s1600/Anh-bia-oto-xe-hop-+(10).jpg" alt="" />
+                <img src="https://lh4.googleusercontent.com/-YqLfRZjymcw/UUvNuqz9esI/AAAAAAAAGng/WGauzgxWRNc/s1600/Anh-bia-oto-xe-hop-+(14).jpg" alt="" />
+            </div>
+        </div>
+    </div>
+    <div class="main">
+        <div class="container">
+            <h2 class="list-car">Danh sách xe ô tô</h2>
+            <div class="row">
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="slider">
-                    <div id="slider-in">
-                        <img src="http://1.bp.blogspot.com/-6eD9x-eISTU/U9x_KcwZV1I/AAAAAAAAOnc/fllCSGEGb-g/s1600/Cover-facebook-mercedes-benz-6.jpg"alt=""/>
-                        <img src="https://lh4.googleusercontent.com/-AwuuI17Lkec/UUvNt-srWfI/AAAAAAAAGnM/x9ZDsWcBegY/s1600/Anh-bia-oto-xe-hop-+(10).jpg"alt=""/>
-                        <img src="https://lh4.googleusercontent.com/-YqLfRZjymcw/UUvNuqz9esI/AAAAAAAAGng/WGauzgxWRNc/s1600/Anh-bia-oto-xe-hop-+(14).jpg"alt=""/>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="session">
+                        <div class="image">
+                            <a href="#">
+                                <img src="./image/hinh-nen-xe-oto-dep-1.jpg" alt="image">
+                            </a>
+                        </div>
+                        <div class="session-title">
+                            <div class="brand">
+                                <a href="#">
+                                    <h2>SUZUKI XL7</h2>
+                                </a>
+                                <p>500.000 VNĐ</p>
+                            </div>
+                            <div class="line"></div>
+                            <div class="title">
+                                Toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc toyota Fortuner 2022 chắc hẳn là cái tên rất quen thuộc
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <form action="" method="POST">
+                    <div class="search">
+                        <div class="search-address">
+                            <p>Chọn địa điểm</p>
+                            <select name="select" id="">
+                                <option value="0">Tỉnh Hà tĩnh</option>
+                                <option value="1">Tỉnh Nghệ An</option>
+                                <option value="2">Tỉnh Thanh Hóa</option>
+                            </select>
+                        </div>
+                        <div class="btn-search">
+                            <input type="submit" value="Tìm kiếm" name="btn-search">
+                        </div>
+                    </div>
+                </form>
+                <h2 class="list-showroom">Danh sách Showroom</h2>
+                <div class="col-6">
+                    <div class="search-infor">
+                        <h2>ShowRoom Hà Tĩnh Media</h2>
+                        <div class="phone">
+                            Điện thoại: <a target="target_blank" href="tel:0123456789">0123456789</a>
+                        </div>
+                        <div class="mail">
+                            Email: <a target="target_blank" href="mailto: nhom@gmail.com">nhom@gmail.com</a>
+                        </div>
+                        <p>Địa chỉ: số 198, Thạch Thành, Thanh Đông, Hà Tĩnh</p>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="search-infor">
+                        <h2>ShowRoom Hà Tĩnh Media1</h2>
+                        <div class="phone">
+                            Điện thoại: <a target="target_blank" href="tel:0123456789">0123456789</a>
+                        </div>
+                        <div class="mail">
+                            Email: <a target="target_blank" href="mailto: nhom@gmail.com">nhom@gmail.com</a>
+                        </div>
+                        <p>Địa chỉ: số 198, Thạch Thành, Thanh Đông, Hà Tĩnh</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="main"></div>
-        <div class="footer"></div>
-    </body>
-    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js" ></script>
-    <script src="./js/jquery-1.12.4.js"></script>
-    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script src="./js/main.js"></script>
+
+    </div>
+    <div class="footer">
+        <img src="https://hondacantho.com/upload/images/slider/050059b6e1fe72a63e92fe003cf9cd5c.jpg" alt="">
+        <h2>Bình Minh công ty dịch vụ bán các dòng xe ô tô cao cấp</h2>
+        <p>công ty Bình Minh là một công ty thuộc tập đoàn VinGroup của Việt Nam được người dân trên toàn thế giới tin tưởng và sử dụng, bởi sự tiên nghi, an toàn và thân thiện</p>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="./js/jquery-1.12.4.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="./js/main.js"></script>
+
 </html>
